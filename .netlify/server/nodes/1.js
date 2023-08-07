c4e48f50daafff9f1d8e51a3c73c099ab5c53f30
Harraz Haziq <97418469+a1phanaught@users.noreply.github.com>
2023-08-07 17:12:34 +0800

    Readded Math.svelte

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.29128f04.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.06801730.js","_app/immutable/chunks/singletons.5af7d92d.js"];
+export const imports = ["_app/immutable/nodes/1.c68587d5.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.06801730.js","_app/immutable/chunks/singletons.6132b416.js"];
 export const stylesheets = [];
 export const fonts = [];
